970b4fb0285e5163143b0f90868852752cf7ebf7
Gaji Mahamud <gaji.mahamud.sst@gmail.com>
2024-03-06 14:07:08 -0500

    Sprites, Movements, and Collisions

---
+++ ship.gd
@@ -0,0 +1,21 @@
+extends Area2D
+
+const LASER_SCENE = preload("res://Laser.tscn")
+
+@export var speed = 100
+@export var armor = 3
+
+func _process(delta):
+	if Input.is_action_pressed("ui_up"):
+		position.y -= speed * delta
+	if Input.is_action_pressed("ui_down"):
+		position.y += speed * delta
+	if Input.is_action_just_pressed("ui_accept"):
+		var laser = LASER_SCENE.instantiate()
+		var world = get_tree().current_scene
+		world.add_child(laser)
+		laser.position = position
+	
+func _on_area_entered(area):
+	queue_free()
+	area.queue_free()
